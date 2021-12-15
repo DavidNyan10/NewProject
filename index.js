7b504e80e61d5426e702711f0cd6f0bcab8d94de
DavidNyan10 <DavidNyan211106@gmail.com>
2021-12-15 09:56:12 +0630

    Thanks Teacher Susan!

--- index.js
+++ index.js
@@ -2,76 +2,80 @@ const fs = require('fs');
 const csv = require('csv-parser')
 
 const myString = new Promise((resolve, reject) => {
-	resolve('John');
+	resolve('Stephen'); 
 });
 
 class myClass{
 	constructor(filename){
 		this.myArray = [];
-		this.myArray.push(this.csvToArray(filename));
+		//this.myArray.push(this.csvToArray(filename));
+		this.csvToArray(filename);
 	}
-    async csvToArray(filename){
-        return await new Promise((res) => {
+	csvToArray(filename){
+		return new Promise((res) => {
 			fs.createReadStream(filename)
 				.pipe(csv(['FirstName', 'LastName', 'Address', 'Town', 'Country', 'Postcode']))
 				.on('data', (data) => {
-				    res(data);
-                    //console.log(data)
+					res(data);
+					this.myArray.push(data);
 				})
-                .on('end', () => {
-                    this.myFunction();
-                });
+				.on('end', (data) => {
+					console.log(JSON.stringify(this.myArray));
+					this.myFunction();
+				  });
 		});
-    }
-	async myFunction() {
-        console.log("=====")
-        console.log(myString);
-        console.log("=====")
-        console.log(this.myArray);
-    	for(let i = 0; i < this.myArray.length; i++) {
-            if (String(myString).includes(this.myArray[i]['FirstName'])){
-                if (this.cooled_down(i)){
-                    this.approve(i, myString);
-                }
-            }
-    	}
+	}
+	myFunction() {
+		console.log("myfunction myString:");
+		console.log(myString);
+		console.log("myfunction myArray:");
+		console.log(this.myArray);
+		for(let i = 0; i < this.myArray.length; i++) {
+			console.log(this.myArray[i]['FirstName']);
+			if (String(myString).includes(this.myArray[i]['FirstName'])){
+				if (this.cooled_down(i)){
+					this.approve(i, myString);
+				}
+			}
+		}
   	}
 	
 	cooled_down(i){
-        dictionary = this.myArray[i]
-        if (!dictionary.keys().includes('approved')){
-            // Means we have never approved on this person!
-            return True;
-        } else{
-            now = datetime.now();
-            duration = now - datetime.fromtimestamp(dictionary['approved']);
-            duration_seconds = duration.total_seconds();
-            hours = divmod(duration_seconds, 3600)[0];
-            if (hours >= 24){
-                return True;
-            } else{
-                console.log("Couldn't approve " + dictionary['FirstName'] + "Cool Down time: " + 24 - hours);
-            }
-        }
-        return False;
-    }
+		var dictionary = this.myArray[i]
+		console.log("cooled_down"+this.myArray[i])
+		if (!dictionary.keys().includes('approved')){
+			// Means we have never approved on this person!
+			return True;
+		} else{
+			now = datetime.now();
+			duration = now - datetime.fromtimestamp(dictionary['approved']);
+			duration_seconds = duration.total_seconds();
+			hours = divmod(duration_seconds, 3600)[0];
+			if (hours >= 24){
+				return True;
+			} else{
+				console.log("Couldn't approve " + dictionary['FirstName'] + "Cool Down time: " + 24 - hours);
+			}
+		}
+		return False;
+	}
 
-    approve(i, myString){
-        dictionary = this.myArray[i];
-        try{
-            setTimeout(function(){
-                console.log(myString);
-                console.log(dictionary['FirstName'])
-                console.log(dictionary['Postcode'])
-            }, 60 * 60 * 3);
-        }catch(e){
-            console.log(e);
-        }
+	approve(i, myString){
+		dictionary = this.myArray[i];
+		try{
+			setTimeout(function(){
+				console.log(myString);
+				console.log(dictionary['FirstName'])
+				console.log(dictionary['Postcode'])
+			}, 60 * 60 * 3);
+		}catch(e){
+			console.log(e);
+		}
 
-        now = datetime.now();
-        this.myArray[i]['approved'] = now.timestamp();
-    }
+		now = datetime.now();
+		this.myArray[i]['approved'] = now.timestamp();
+	}
 }
 
 myObj = new myClass("myCSV.csv");
-myString.then( () => myObj.myFunction());
+//myString.then( () => myObj.myFunction());
